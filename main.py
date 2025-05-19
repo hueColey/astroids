1f6f1b7979790fae2c6dcf2bc25c81136d07c285
HueColey <Coley4dev@gmail.com>
2025-05-19 17:24:00 -0500

    rebase atroids change astroids to ssh in console

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame
+import circleshape
+import player
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
 
+    player_ship = player.Player(x, y)
 
     while True:
         for event in pygame.event.get():
@@ -12,8 +19,9 @@ def main():
                 return 
 
         screen.fill("black")
+        player_ship.draw(screen) 
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 
 
