245fd69c38cccdf0b6d38e761a7c4340af5b62ef
HueColey <Coley4dev@gmail.com>
2025-05-19 19:14:12 -0500

    added player movement and updated player.Player.containers

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import circleshape
 import player
 from constants import *
 
+updateable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+
+player.Player.containers = (updateable, drawable)  
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -18,8 +24,11 @@ def main():
             if event.type == pygame.QUIT:
                 return 
 
-        screen.fill("black")
-        player_ship.draw(screen) 
+        screen.fill("black") 
+        updateable.update(dt)
+        for thing in drawable:
+                thing.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
